799164cbeaf5fc5a73387259aafb31561c0ae652
todorilievski <todor.ilievski@byteworx.de>
2023-02-12 19:03:23 +0100

    add account Service tests

--- src/test/java/api/customers/service/AccountServiceTest.java
+++ src/test/java/api/customers/service/AccountServiceTest.java
@@ -1,10 +1,15 @@
 package api.customers.service;
 
+import api.customers.entity.Account;
 import api.customers.repository.AccountRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -15,6 +20,18 @@ public class AccountServiceTest {
 
     @InjectMocks
     private AccountService accountService;
+
+    @Test
+    public void testOpenAccount() {
+        int customerId = 1;
+
+        Account expectedAccount = new Account(customerId);
+
+        when(accountRepository.createAccount(customerId)).thenReturn(expectedAccount);
+
+        Account actualAccount = accountService.createAccount(customerId);
+        assertEquals(expectedAccount.getAccountNumber(), actualAccount.getAccountNumber(), 0);
+    }
 
 
 }
