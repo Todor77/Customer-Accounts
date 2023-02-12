7d47b84ec6b210e3ea401c19576e1225d48dc1ca
todorilievski <todor.ilievski@byteworx.de>
2023-02-12 12:27:50 +0100

    Exception implementation

--- src/main/java/api/customers/exception/RestException.java
+++ src/main/java/api/customers/exception/RestException.java
@@ -1,4 +1,23 @@
 package api.customers.exception;
 
-public class RestException {
+
+public class RestException extends RuntimeException{
+
+    private int code;
+
+    public RestException() {
+    }
+
+    public RestException(int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }
